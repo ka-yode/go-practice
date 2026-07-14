da2b96e53e5036c858788878061cecabcd3dd3a0
Kayode Babalola <babalolakayode44@gmail.com>
2026-07-14 09:36:10 +0100

    new managers

---
+++ price-calculator/cmdmanager/cmdmanager.go
@@ -0,0 +1,29 @@
+package cmdmanager
+
+import "fmt"
+
+type CMDManager struct {
+}
+
+func (cmd CMDManager) ReadLines() ([]string, error) {
+	var prices []string
+
+	for {
+		var price string
+		fmt.Println("Enetr your prices")
+		fmt.Scan(&price)
+		if price == "0" {
+			break
+		}
+		prices = append(prices, price)
+	}
+	return prices, nil
+}
+
+func (cmd CMDManager) WriteResult(data any) error {
+	fmt.Println(data)
+	return nil
+}
+func New() CMDManager {
+	return CMDManager{}
+}
